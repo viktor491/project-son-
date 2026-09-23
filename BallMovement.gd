e9a1897b9957c93923a909bcabb8a63f5cb7346d
viktor491 <x9686673@gmail.com>
2026-09-23 12:15:01 -0600

    Walls

--- BallMovement.gd
+++ BallMovement.gd
@@ -12,21 +12,44 @@ func _ready() -> void:
 	initialHorizontalSpeed = horizontalSpeed
 	initialVerticalSpeed = verticalSpeed
 	
-	pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _process(delta):
 	if (firstCollision == false):
 		horizontalSpeed = 0
 	
 	var collision = move_and_collide(Vector2(horizontalSpeed * delta, verticalSpeed * -dir * delta)) 
+	var playerDir = Input.get_axis("ui_left","ui_right")
 	
 	if(collision):
 		firstCollision = true 
 		var colliderName = collision.get_collider().name
+		print(colliderName)
 		
 		if (colliderName == "Player"):
 			dir = 1
-			horizontalSpeed = randi_range(-initialHorizontalSpeed,initialHorizontalSpeed)		
-	pass			
+			horizontalSpeed = abs(horizontalSpeed) * playerDir
+			
+		if (colliderName == "TopWall"):
+			dir = -1
+			horizontalSpeed = randi_range(-initialHorizontalSpeed,initialHorizontalSpeed)
+			verticalSpeed = randi_range(initialVerticalSpeed / 1.3, verticalSpeed * 2)
+			print("TOPWALL")	
+		
+		if (colliderName == "BottomWall"):
+			dir = 1
+			horizontalSpeed = randi_range(-initialHorizontalSpeed,initialHorizontalSpeed)
+			verticalSpeed = randi_range(initialVerticalSpeed / 1.3, initialVerticalSpeed * 2)		
+		
+		if (colliderName == "LeftWall"):
+			dir = -1
+			horizontalSpeed = randi_range(initialHorizontalSpeed / 1.3,initialHorizontalSpeed)
+			verticalSpeed = randi_range(initialVerticalSpeed / 1.3, initialVerticalSpeed * 2)	
+		
+		if (colliderName == "RightWall"):
+			dir = -1
+			horizontalSpeed = randi_range(-initialHorizontalSpeed / 1.3,-initialHorizontalSpeed)
+			verticalSpeed = randi_range(initialVerticalSpeed / 1.3, initialVerticalSpeed * 2)	
+				
